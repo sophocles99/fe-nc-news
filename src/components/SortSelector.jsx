90abe62c1fd268db3980b06afe425863786ccd32
sophocles99 <22369144+sophocles99@users.noreply.github.com>
2023-07-20 13:54:24 +0100

    ticket10 - sort articles

--- src/components/SortSelector.jsx
+++ src/components/SortSelector.jsx
@@ -1,5 +1,48 @@
-const SortSelector = () => {
-  return <select></select>
+import { FaSortAmountUp, FaSortAmountDown } from "react-icons/fa";
+
+const SortSelector = ({ sortBy, setSortBy, order, setOrder }) => {
+  const sortByOptions = [
+    { value: "", text: "Sort By" },
+    { value: "created_at", text: "Date" },
+    { value: "comment_count", text: "Comments" },
+    { value: "votes", text: "Votes" },
+  ];
+  if (!sortBy) sortBy = "";
+
+  const changeSortBy = (newSortBy) => {
+    if (newSortBy === "") newSortBy = null;
+    setSortBy(newSortBy);
+  };
+
+  return (
+    <div className="sort-selector">
+      <select
+        value={sortBy}
+        onChange={(e) => changeSortBy(e.target.value)}
+        className="sort-selector-select"
+      >
+        {sortByOptions.map((sortByOption) => {
+          return (
+            <option value={sortByOption.value} key={sortByOption.value}>
+              {sortByOption.text}
+            </option>
+          );
+        })}
+      </select>
+      <button
+        className="sort-order-button ascending"
+        onClick={() => setOrder("asc")}
+      >
+        {FaSortAmountUp()}
+      </button>
+      <button
+        className="sort-order-button descending"
+        onClick={() => setOrder("desc")}
+      >
+        {FaSortAmountDown()}
+      </button>
+    </div>
+  );
 };
 
 export default SortSelector;
